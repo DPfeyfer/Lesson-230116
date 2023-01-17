4eacdb140bfde3ed4d262383c1cd8ded0f00afb5
DPfeyfer <Skydenfer737@gmail.com>
2023-01-17 15:10:09 +0300

    learn and test

---
+++ src/For_test.java
@@ -0,0 +1,17 @@
+public class For_test {
+    public static void main(String[] args) {
+        int[] weights = new int[12];
+        weights[0] = 92;
+        weights[1] = 93;
+        weights[2] = 95;
+        weights[3] = 94;
+        weights[11] = 96;
+        int januaryWeight = weights[1];
+        System.out.println(januaryWeight);
+        System.out.println(weights[2]);
+        System.out.println(weights[3]);
+        int january = 11;
+        System.out.println(weights[january]);
+
+    }
+}
