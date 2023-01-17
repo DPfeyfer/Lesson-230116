502bc8f1f86cec3689095e2ce2e3f587587725f9
DPfeyfer <Skydenfer737@gmail.com>
2023-01-17 15:28:57 +0300

    complete learn and test

--- src/For_test.java
+++ src/For_test.java
@@ -1,5 +1,6 @@
 public class For_test {
     public static void main(String[] args) {
+        //int[] weights = {92, 93, 95, 94, 90, 89, 85, 0, 0, 0, 0, 0 };
         int[] weights = new int[12];
         weights[0] = 92;
         weights[1] = 93;
@@ -11,7 +12,14 @@ public class For_test {
         System.out.println(weights[2]);
         System.out.println(weights[3]);
         int january = 11;
-        System.out.println(weights[january]);
+        System.out.println(weights[january]);  //How to do new PSVM ?
+        System.out.println();
 
+
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
+        System.out.println();
+        System.out.println(weights.length);
     }
 }
