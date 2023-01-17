5297731f64fc3cce3f69a9a184d0d0dcb760e6d3
DPfeyfer <Skydenfer737@gmail.com>
2023-01-17 15:33:35 +0300

    Started doing task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Task 1
+        System.out.println("Task 1");
+        System.out.println();
+
+        //Task 2
+        System.out.println("Task 2");
+        System.out.println();
+
+        //Task 3
+        System.out.println("Task 3");
+        System.out.println();
+
+        //Task 4
+        System.out.println("Task 4");
+        System.out.println();
     }
 }
