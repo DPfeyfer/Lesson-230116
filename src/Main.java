e7a72e0f4197ad6a97cca28ee30e98ea369c57df
DPfeyfer <Skydenfer737@gmail.com>
2023-01-18 12:33:42 +0300

    Task 1 complete

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,24 @@ public class Main {
 
         //Task 1
         System.out.println("Task 1");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        System.out.println(number[1]);
+        int[] number1 = new int[3];
+        number1[0] = 1;
+        number1[1] = 2;
+        number1[2] = 3;
+        int forUnderstand = number1[2];
+        int[] number2 = new int[3];
+        number2[0] = 1;
+        number2[1] = 2;
+        number2[2] = 3;
+        int forUnderstand1 = 0;
+        System.out.println(number2[forUnderstand1]);
+
+        System.out.println(forUnderstand);
         System.out.println();
 
         //Task 2
